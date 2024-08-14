b06859c0a10794922c2f3251368643cb239168eb
Bensoli-Inc <benswambua96@gmail.com>
2024-08-14 09:12:41 +0300

    variables

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class Hello
+{
+    public static void main(String[] args) 
+    {   
+        int num = 5;
+        int num2 = 10;
+        int result = num + num2;
+         System.out.println(result);
+    }
+}
