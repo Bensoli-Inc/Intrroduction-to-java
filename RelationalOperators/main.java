36ea1433cc2a0c7e4373b30c179c6e10a17891db
Bensoli-Inc <benswambua96@gmail.com>
2024-08-17 19:48:56 +0300

    relational ops

---
+++ RelationalOperators/main.java
@@ -0,0 +1,18 @@
+class Main 
+{
+    public static void main(String[] args) 
+    {
+        int x= 6;
+        int y=6;
+
+        int a = 8;
+        int b= 9;
+        
+        boolean result = x < y;
+        boolean result1 = x <= y;
+        boolean result2 = x == y;
+        boolean result2 = x != y;
+
+        System.out.println(result2);
+    }
+}
