015331232a889fb796d2823f4f58cfceeec3b02f
Bensoli-Inc <benswambua96@gmail.com>
2024-08-18 10:55:21 +0300

    conditional statements

--- ConditionalStatements/Main.java
+++ ConditionalStatements/Main.java
@@ -3,7 +3,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        //FIRST STATEMENT
         int x = 28;
         
         if(x>10 && x<=20)
@@ -12,6 +12,7 @@ public class Main
           System.out.println("Bye");
 
         
+        //SECOND STATEMENT
         int a = 5;
         int b = 7;
 
@@ -22,5 +23,24 @@ public class Main
         }
         else
         System.out.println(b);
+
+        //THIRD STATEMENT
+        int i = 8;
+        int j = 7;
+        int k = 9;
+
+        if(i>j && i>k)
+        {
+            System.out.println(i);
+            System.out.println(i + " is greatest");
+        }
+        else if (j>k) {
+            System.out.println(j);
+            System.out.println(j + " is greatest");
+        }
+        else
+            System.out.println(k);
+            System.out.println(k + " is greatest");
+        
     }
 }
