c6dd3a12a36ea788207b3ab400da2cef1d6d255f
Bensoli-Inc <benswambua96@gmail.com>
2024-08-18 10:42:04 +0300

    if else

---
+++ ConditionalStatements/Main.java
@@ -0,0 +1,26 @@
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        int x = 28;
+        
+        if(x>10 && x<=20)
+          System.out.println("Hello");
+        else
+          System.out.println("Bye");
+
+        
+        int a = 5;
+        int b = 7;
+
+        if(a>b)
+        {
+            System.out.println(a);
+            System.out.println("Thank you");
+        }
+        else
+        System.out.println(b);
+    }
+}
