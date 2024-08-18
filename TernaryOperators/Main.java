c29f9517a2e9c5a314e5ede7b3aad7e80ae3c8df
Bensoli-Inc <benswambua96@gmail.com>
2024-08-18 11:07:34 +0300

    ternary ? :

---
+++ TernaryOperators/Main.java
@@ -0,0 +1,12 @@
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        int n =7;
+        String result = "number";
+
+        result = n%2==0 ? "even" : "odd";
+
+        System.out.println(result);
+    }
+}
