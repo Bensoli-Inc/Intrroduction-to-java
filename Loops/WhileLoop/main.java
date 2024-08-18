b40d0a328c892e9d7a87afa7274118306417e5dc
Bensoli-Inc <benswambua96@gmail.com>
2024-08-18 12:01:45 +0300

    while loop

---
+++ Loops/WhileLoop/main.java
@@ -0,0 +1,17 @@
+
+public class Main 
+{
+    public static void main(String[] a)
+    {   
+        int i=0;
+
+        while(i<5)
+        {
+            System.out.println("Hi " + i);
+            i++;
+        }
+
+        System.out.println("Bye " + i);
+        
+    }
+}
