a2aff74eb2609a9237235fdac4a5ce9a24f98bad
Bensoli-Inc <benswambua96@gmail.com>
2024-08-18 11:34:33 +0300

    switch case break

---
+++ ConditionalStatements/SwitchCases/Main.java
@@ -0,0 +1,35 @@
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        int n = 8;
+
+        switch(n) {
+            case 1:
+                System.out.println("Monday");
+            break;
+            case 2:
+                System.out.println("Tuesday");
+            break;
+            case 3:
+                System.out.println("Wednesday");
+            break;
+            case 4:
+                System.out.println("Thursday");
+            break;
+            case 5:
+                System.out.println("Friday");
+            break;
+            case 6:
+                System.out.println("Saturday");
+            break;
+            case 7:
+                System.out.println("Sunday");
+            break;
+            default:
+                System.out.println("Not a day");
+
+        }
+
+    }
+}
