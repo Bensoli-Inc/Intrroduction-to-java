5ac2f3da57ce3cc2be384a0a1098d5e4bc79a9a8
Bensoli-Inc <benswambua96@gmail.com>
2024-08-19 07:54:36 +0300

    declaring string

---
+++ Strings/main.java
@@ -0,0 +1,16 @@
+public class Main 
+{
+    public static void main(String[] a)
+    {
+        String name = new String("ben");
+        System.out.println(name);
+
+        System.out.println("Hello " + name); //joining with another string
+
+        //the class String has varius methods eg:
+        System.out.println(name.charAt(0));
+        System.out.println(name.concat(" sasa")); //concatinating 2strings- joining
+
+
+    }
+}
